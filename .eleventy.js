bbb139e294826458397dbef6eed9c01dc55485f5
Suresh kumara <kumara.suresh25@gmail.com>
2025-10-12 20:30:38 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,11 @@ module.exports = function(eleventyConfig) {
   
     // Set global data for languages
     eleventyConfig.addGlobalData("languages", require("./src/_data/languages.json"));
-  
+
+    eleventyConfig.addGlobalData("site", {
+      date: new Date()
+    });
+
     // Passthrough assets like images or CSS files
     eleventyConfig.addPassthroughCopy("src/assets");
   
